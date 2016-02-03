2342bfe3594f1f0f500b2de4f8aadfc66a52fc9c
Chris Thoburn <runspired@gmail.com>
2016-02-03 17:48:21 -0600

    removes logging

--- htmlbars-plugins/component-conversion.js
+++ htmlbars-plugins/component-conversion.js
@@ -13,39 +13,17 @@ function ComponentConversionSupport() {
   this.syntax = null;
 }
 
-/*
- function buildBlock(path, params, hash, program, inverse, loc) {
- return {
- type: "BlockStatement",
- path: buildPath(path),
- params: params || [],
- hash: hash || buildHash([]),
- program: program || null,
- inverse: inverse || null,
- loc: buildLoc(loc)
- };
- }
- */
-
 ComponentConversionSupport.prototype.transform = function ComponentConversionSupport_transform(ast) {
   var b = this.syntax.builders;
   var pluginContext = this;
   var walker = new pluginContext.syntax.Walker();
 
   walker.visit(ast, function(element) {
-    if (['BlockStatement'].indexOf(element.type) !==  -1) {
-      console.log('\n\n\n\nOriginal Block\n');
-      console.log(element.hash);
-      console.log('\n\n\n\nOriginal Block\n');
-      console.log(element.hash.pairs);
-      console.log('\n\n\n\nOriginal Block\n');
-      console.log(element.hash.pairs[0]);
-    }
     if (pluginContext.validate(element)) {
       var program = b.program(element.children);
       var tag = ComponentPrefix + element.tag;
       var component = b.block(tag, null, makeHash(element.attributes), program, null, element.loc);
-      console.log(component);
+
       replaceReference(element, component);
     }
   });
@@ -61,17 +39,7 @@ function replaceReference(a, b) {
     a[key] = b[key];
   });
 }
-// [ { type: 'AttrNode', name: 'class', value: [Object] } ]
-/*
- [ { loc: SourceLocation { source: undefined, start: [Object], end: [Object] },
- type: 'HashPair',
- key: 'class',
- value:
- { loc: [Object],
- type: 'StringLiteral',
- value: 'foo',
- original: 'foo' } } ]
- */
+
 function makeHash(attrs) {
   if (!attrs || !attrs.length) {
     return {};
