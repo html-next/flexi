25f236e2c1b71f6cfc2d63adc4b21f37179f6142
Chris Thoburn <runspired@gmail.com>
2016-02-05 01:47:02 -0600

    chore(cruft): removes unused helpers

--- addon/helpers/structure.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function structure(params/*, hash*/) {
-  return params;
-}
-
-export default Ember.Helper.helper(structure);
--- app/helpers/structure.js
+++
@@ -1,1 +0,0 @@
-export { default, structure } from 'flexi/helpers/structure';
