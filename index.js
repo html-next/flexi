0baae1545681773361f753be1c34da6763a90d78
Chris Thoburn <runspired@gmail.com>
2016-02-20 01:50:48 -0600

    makes breakpoints configurable for the service and attribute conversion

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var LayoutCompiler = require('./lib/layout-compiler');
 var mergeTrees = require('broccoli-merge-trees');
 // var log = require('broccoli-stew').log;
 var Funnel = require('broccoli-funnel');
+var path = require('path');
+var fs = require('fs');
 
 module.exports = {
   name: 'flexi',
@@ -24,10 +26,26 @@ module.exports = {
     return true;
   },
 
+  config(env, config) {
+    this._super.config.apply(this, arguments);
+
+    var configPath = path.join(this.project.root, 'config', 'flexi.js');
+
+    if ((!this.app.options || !this.app.options.flexi) && fs.existsSync(configPath)) {
+      var flexiConfig = require(configPath);
+      this.app.options = this.app.options || {};
+      this.app.options.flexi = flexiConfig;
+      config.APP.flexi = this.app.options.flexi;
+    }
+
+  },
+
   setupPreprocessorRegistry: function(type, registry) {
     var AttributeConversion = require('./htmlbars-plugins/attribute-conversion');
     var ComponentConversion = require('./htmlbars-plugins/component-conversion');
     var SustainConversion = require('./htmlbars-plugins/sustain-conversion');
+
+    AttributeConversion.LayoutSizes = getLayoutSizes(this.app.options.flexi.breakpoints);
 
     registry.add('htmlbars-ast-plugin', {
       name: "flexi-attribute-conversion",
@@ -51,7 +69,7 @@ module.exports = {
       if (!tree) {
         throw new Error("No Template Tree is Present");
       }
-      var layoutTree = new LayoutCompiler(tree);
+      var layoutTree = new LayoutCompiler(tree, { breakpoints: this.app.options.flexi.breakpoints });
       var templateTree = new Funnel(tree, {
         exclude: ['**/layouts/*.hbs']
       });
@@ -62,3 +80,9 @@ module.exports = {
   }
 
 };
+
+function getLayoutSizes(breakpoints) {
+  return breakpoints.map(function(bp) {
+    return bp.prefix;
+  });
+}
