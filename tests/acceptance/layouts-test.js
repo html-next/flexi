1bf8d725a91276f2332dd89d8e90feaba8750175
Chris Thoburn <runspired@gmail.com>
2016-02-21 13:58:40 -0600

    fix(breakpoints-test): breakpoints are now better defined

--- tests/acceptance/layouts-test.js
+++ tests/acceptance/layouts-test.js
@@ -18,7 +18,7 @@ test('visiting /layout-test', function(assert) {
   let breakpoints = deviceLayout.get('breakpoints');
   let bp = {};
   breakpoints.forEach(function(point) {
-    bp[point.name] = point.begin + 100;
+    bp[point.name] = point.begin + 5;
   });
 
   deviceLayout.set('width', bp.huge);
