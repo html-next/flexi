7bca5f140133e6880a3c3ad8fd22060a9a646d10
Chris Thoburn <runspired@gmail.com>
2016-02-22 13:02:17 -0600

    layout generation start

---
+++ blueprints/route/index.js
@@ -0,0 +1,5 @@
+/*jshint node:true*/
+var dcb = require('ember-cli/blueprints/component/index.js');
+var makeLayoutAware = require('../../lib/blueprints/make-layout-aware');
+
+module.exports = makeLayoutAware(dcb, 'route');
