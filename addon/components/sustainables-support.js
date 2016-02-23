f8db6d5c2ae91bb62173ebd2c5a3cb854f05b814
Chris Thoburn <runspired@gmail.com>
2016-02-23 15:39:41 -0600

    we don't actually need to render individual items into fragments anymore

--- addon/components/sustainables-support.js
+++ addon/components/sustainables-support.js
@@ -1,25 +1,16 @@
 import Ember from 'ember';
 import layout from '../templates/components/sustainables-support';
-import FragmentRendering from '../mixins/render-into-fragment';
 
 const {
   Component,
   computed,
-  Handlebars,
   inject
   } = Ember;
 
-const {
-  SafeString
-  } = Handlebars;
-
-export default Component.extend(FragmentRendering, {
+export default Component.extend({
   layout,
-  tagName: 'sustains-support',
   '-sustains': inject.service('-sustains'),
   sustains: computed.alias('-sustains._sustains'),
-  attributeBindings: ['style'],
-  style: new SafeString('display: none; width: 0; height: 0; opacity: 0; visibility: hidden;'),
 
   didInsertElement() {
     this._super();
