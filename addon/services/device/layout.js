e87c721b7c01553ad4c43ea68f6697d569a09d1f
Chris Thoburn <runspired@gmail.com>
2016-02-25 09:08:14 -0600

    feat(orientation): adds orientation to layout service

--- addon/services/device/layout.js
+++ addon/services/device/layout.js
@@ -2,13 +2,23 @@ import Ember from 'ember';
 
 const {
   computed,
-  Service
+  Service,
+  run
   } = Ember;
 
 export default Service.extend({
 
   width: 1000,
   height: 500,
+
+  orientation: computed('width', 'height', function() {
+    let resolution = this.getProperties('width', 'height');
+    let isLandscape = resolution.width >= resolution.height;
+    return isLandscape ? 'landscape' : 'portrait';
+  }).readOnly(),
+
+  deviceIsLandscape: computed.equal('orientation', 'landscape'),
+  deviseIsPortrait: computed.not('deviceIsLandscape'),
 
   breakpoints: null,
   _resizeHandler: null,
@@ -18,17 +28,20 @@ export default Service.extend({
     window.removeEventListener('resize', this._resizeHandler, true);
   },
 
+  getResolution() {
+    let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    this.setProperties({
+      width: w,
+      height: h
+    });
+  },
+
   setupResize() {
     this._resizeHandler = () => {
-      let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-      let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-      this.setProperties({
-        width: w,
-        height: h
-      });
+      run.debounce(this, this.getResolution, 16);
     };
     window.addEventListener('resize', this._resizeHandler, true);
-    this._resizeHandler();
   },
 
   setupBreakpoints() {
@@ -54,6 +67,7 @@ export default Service.extend({
 
     this.setupBreakpoints();
     this.setupResize();
+    this.getResolution();
   }
 
 });
