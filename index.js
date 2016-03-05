a7c00a527f1a4450ccd2be0a44a69ebd3a0a5281
Chris Thoburn <runspired@gmail.com>
2016-03-04 18:09:31 -0600

    remove log

--- index.js
+++ index.js
@@ -43,8 +43,6 @@ module.exports = {
       if (fs.existsSync(configPath)) {
         this._flexiConfig = require(configPath);
 
-        console.log('ARGS!', process.argv);
-
         assert("config/flexi.js is defined, but could not be imported", this._flexiConfig);
         assert("config/flexi.js is defined, but did not contain property [array] breakpoints", this._flexiConfig.breakpoints instanceof Array);
         assert("config/flexi.js is defined, but did not contain property [number] columns", typeof this._flexiConfig.columns === 'number');
