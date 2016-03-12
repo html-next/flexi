f28f18ac675f6f9d60fed94692722d273de8afd5
Chris Thoburn <runspired@gmail.com>
2016-03-12 01:27:58 -0600

    remove forgotten logs

--- addon/utils/dom/append-cached-range.js
+++ addon/utils/dom/append-cached-range.js
@@ -3,18 +3,9 @@ export default function appendCachedRange(element, elementList) {
   let lastElement = element.lastChild || element.lastNode;
   let parent = lastElement ? lastElement.parentNode : element;
 
-  console.log('---- PARENT ----');
-  console.dir(parent);
-  console.log('---- LOCATION ----');
-  console.dir(lastElement);
-  console.log('---- APPENDING ----');
-
   for (let i = 0; i < elementList.length; i++) {
-    console.dir(elementList[i]);
     parent.insertBefore(elementList[i], lastElement);
   }
-
-  console.log('____ FIN ____');
 
   if (document.activeElement !== currentActiveElement) {
     currentActiveElement.focus();
