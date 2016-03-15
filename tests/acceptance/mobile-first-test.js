84d415c52861b1515b76b8b3f549d6d8320e8420
Eli Dupuis <eli@showbie.com>
2016-03-15 14:08:25 -0600

    Add breaking mobile-first test
    This demonstrates how a larger breakpoint layout (desktop) is rendered when a
    smaller layout (mobile) should be rendered to follow the mobile-first paradigm.
    
    Note that in this example the active breakpoint (tablet) has no layout defined.

---
+++ tests/acceptance/mobile-first-test.js
@@ -0,0 +1,51 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import Ember from 'ember';
+
+function getOwner(context) {
+  return context.application.__deprecatedInstance__ || context.application.__container__;
+}
+
+const {
+  run
+  } = Ember;
+
+moduleForAcceptance('Acceptance | mobile-first', {
+  beforeEach(assert) {
+    assert.deviceLayout = getOwner(this).lookup('service:device/layout');
+  }
+});
+
+test('visiting /tests/mobile-first', function(assert) {
+  visit('/tests/mobile-first');
+  let { deviceLayout } = assert;
+  let breakpoints = deviceLayout.get('breakpoints');
+  let bp = {};
+  breakpoints.forEach(function(point) {
+    bp[point.name] = point.begin + 5;
+  });
+
+  deviceLayout.set('width', bp.mobile);
+
+  andThen(function() {
+    assert.equal(currentURL(), '/tests/mobile-first');
+
+    assert.equal(find('h1.layout-test').text(), 'Mobile!', `The layout renders the mobile layout when width is ${bp.mobile}`);
+    run(() => {
+      deviceLayout.set('width', bp.tablet);
+    });
+
+    andThen(() => {
+      assert.equal(find('h1.layout-test').text(), 'Mobile!', `The layout still renders the mobile layout when width is ${bp.tablet} (no tablet layout defined)`);
+      run(() => {
+        deviceLayout.set('width', bp.desktop);
+      });
+
+      andThen(() => {
+        assert.equal(find('h1.layout-test').text(), 'Desktop!', `The layout renders the desktop layout when width is ${bp.desktop}`);
+
+      });
+    });
+
+  });
+});
