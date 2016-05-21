891a30c39097b31ee91014a07eaa8930a6ad15a3
Chris Thoburn <runspired@gmail.com>
2016-05-21 00:24:43 -0500

    fix engines compat

--- index.js
+++ index.js
@@ -24,16 +24,26 @@ function assert(statement, test) {
 module.exports = {
   name: 'flexi',
 
-  included: function(app) {
+  included: function(app, parentAddon) {
     this._super.included.apply(this, arguments);
 
+    // Quick fix for add-on nesting
+    // https://github.com/aexmachina/ember-cli-sass/blob/v5.3.0/index.js#L73-L75
     // see: https://github.com/ember-cli/ember-cli/issues/3718
-    while (typeof app.import !== 'function' && app.app) {
-      app = app.app;
+    while (typeof app.import !== 'function' && (app.app || app.parent)) {
+      app = app.app || app.parent;
     }
 
-    if (typeof app.import !== 'function') {
-      throw new Error('Flexi is being used within another addon or engine and is' +
+    // if app.import and parentAddon are blank, we're probably being consumed by an in-repo-addon
+    // or engine, for which the "bust through" technique above does not work.
+    if (typeof app.import !== 'function' && !parentAddon) {
+      if (app.registry && app.registry.app) {
+        app = app.registry.app;
+      }
+    }
+
+    if (!parentAddon && typeof app.import !== 'function') {
+      throw new Error('ember-font-awesome is being used within another addon or engine and is' +
         ' having trouble registering itself to the parent application.');
     }
 
