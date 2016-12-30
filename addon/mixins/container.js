56d6ec4b734e547e8771d49caf080ca68c2f123a
Kyle Bishop <kybishop@gmail.com>
2016-12-31 00:39:09 +0700

    fix(ResizeMonitor) don't monitor null elements

--- addon/mixins/container.js
+++ addon/mixins/container.js
@@ -9,6 +9,8 @@ const {
 
 export default Mixin.create({
   deviceLayout: inject.service('device/layout'),
+
+  _afterRenderTask: null,
   width: 0,
   inserted: false,
   classNameBindings: ['breakpointClass'],
@@ -19,13 +21,14 @@ export default Mixin.create({
      return `container-${bps[0].prefix}`;
     }
 
-    let width = this.element.clientWidth;
+    let width = this.get('element').clientWidth;
 
     for (let i = 0; i < bps.length; i++) {
       if (width >= bps[i].begin) {
         return `container-${bps[i].prefix}`;
       }
     }
+
     return 'container-breakpoint-unavailable';
   }),
 
@@ -38,20 +41,25 @@ export default Mixin.create({
     this._super();
     this._elementResize = this.elementResize.bind(this);
 
-    run.schedule('afterRender', () => {
+    let afterRenderTask = run.schedule('afterRender', () => {
       this.set('inserted', true);
       this.get('deviceLayout')
         .monitor
-        .addElementHandler(this.element, this._elementResize);
+        .addElementHandler(this.get('element'), this._elementResize);
     });
+
+    this.set('_afterRenderTask', afterRenderTask);
   },
 
   willDestroyElement() {
     this._super();
+
+    run.cancel(this.get('_afterRenderTask'));
+
     this.set('inserted', false);
     this.get('deviceLayout')
       .monitor
-      .removeElementHandler(this.element, this._elementResize);
+      .removeElementHandler(this.get('element'), this._elementResize);
     this._elementResize = null;
   }
 });
