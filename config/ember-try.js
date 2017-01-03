3ba94d40cd161941ad02b9e48b2e50f913912e8f
Robert Wagner <rwwagner90@gmail.com>
2017-01-03 12:34:25 -0500

    Add LTS versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,7 +5,6 @@
  template compiler cannot handle flexi.
  */
 module.exports = {
-  useVersionCompatibility: true,
   scenarios: [
     {
       name: 'ember-1.13.13',
@@ -19,13 +18,24 @@ module.exports = {
       }
     },
     {
-      name: 'ember-2.1.2',
+      name: 'ember-lts-2.4',
       bower: {
         dependencies: {
-          'ember': '~2.1.2'
+          'ember': 'components/ember#lts-2-4'
         },
         resolutions: {
-          'ember': '~2.1.2'
+          'ember': 'lts-2-4'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.8',
+      bower: {
+        dependencies: {
+          'ember': 'components/ember#lts-2-8'
+        },
+        resolutions: {
+          'ember': 'lts-2-8'
         }
       }
     },
@@ -42,7 +52,6 @@ module.exports = {
     },
     {
       name: 'ember-beta',
-      "allowedToFail": false,
       bower: {
         dependencies: {
           'ember': 'components/ember#beta'
@@ -54,7 +63,6 @@ module.exports = {
     },
     {
       name: 'ember-canary',
-      "allowedToFail": true,
       bower: {
         dependencies: {
           'ember': 'components/ember#canary'
