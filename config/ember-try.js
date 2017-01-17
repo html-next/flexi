a849cb2e231f4589537e69b1546320c0801adbb0
Robert Wagner <rwwagner90@gmail.com>
2017-01-16 22:07:01 -0500

    Ember init

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,12 +5,14 @@
  template compiler cannot handle flexi.
  */
 module.exports = {
+  useVersionCompatibility: true,
   scenarios: [
     {
       name: 'ember-1.13.13',
       bower: {
         dependencies: {
-          'ember': '~1.13.13'
+          'ember': '~1.13.13',
+          'ember-cli-shims': '0.1.1'
         },
         resolutions: {
           'ember': '~1.13.13'
@@ -21,10 +23,16 @@ module.exports = {
       name: 'ember-lts-2.4',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-4'
+          'ember': 'components/ember#lts-2-4',
+          'ember-cli-shims': '0.1.3'
         },
         resolutions: {
           'ember': 'lts-2-4'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
         }
       }
     },
@@ -32,10 +40,16 @@ module.exports = {
       name: 'ember-lts-2.8',
       bower: {
         dependencies: {
-          'ember': 'components/ember#lts-2-8'
+          'ember': 'components/ember#lts-2-8',
+          'ember-cli-shims': '0.1.3'
         },
         resolutions: {
           'ember': 'lts-2-8'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
         }
       }
     },
@@ -43,10 +57,16 @@ module.exports = {
       name: 'ember-release',
       bower: {
         dependencies: {
-          'ember': 'components/ember#release'
+          'ember': 'components/ember#release',
+          'ember-cli-shims': '0.1.3'
         },
         resolutions: {
           'ember': 'release'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
         }
       }
     },
@@ -54,10 +74,16 @@ module.exports = {
       name: 'ember-beta',
       bower: {
         dependencies: {
-          'ember': 'components/ember#beta'
+          'ember': 'components/ember#beta',
+          'ember-cli-shims': '0.1.3'
         },
         resolutions: {
           'ember': 'beta'
+        }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
         }
       }
     },
@@ -65,11 +91,23 @@ module.exports = {
       name: 'ember-canary',
       bower: {
         dependencies: {
-          'ember': 'components/ember#canary'
+          'ember': 'components/ember#canary',
+          'ember-cli-shims': '0.1.3'
         },
         resolutions: {
           'ember': 'canary'
         }
+      },
+      npm: {
+        devDependencies: {
+          'ember-source': null
+        }
+      }
+    },
+    {
+      name: 'ember-default',
+      npm: {
+        devDependencies: {}
       }
     }
   ]
