3d2be32c58ea3ef0eeabe78164ec976b7588f64a
Robert Wagner <rwwagner90@gmail.com>
2017-01-17 12:27:10 -0500

    Add basic prompt to choose add ons to install

--- blueprints/flexi/index.js
+++ blueprints/flexi/index.js
@@ -1,16 +1,54 @@
 /*jshint node:true*/
 module.exports = {
-  description: 'Installs config',
+  description: 'Choose which addons to install',
 
-  normalizeEntityName: function() {},
+  normalizeEntityName: function() {
+  },
 
   afterInstall: function(options) {
-    return this.addAddonsToProject({
-      packages: [
-        { name: 'flexi-default-dsl' },
-        { name: 'flexi-default-styles' },
-        { name: 'flexi-layouts' }
+    return this._chooseAddonsToInstall()
+      .then((addons) => {
+        return this.addAddonsToProject({
+          packages: addons
+        });
+      });
+  },
+  /**
+   * Uses inquirer to prompt the user to select which addons to install
+   * @returns {Promise} Resolves into array of selected addons
+   * @private
+   */
+  _chooseAddonsToInstall: function() {
+    // Ask which ember addons to install
+    return this.ui.prompt({
+      type: 'checkbox',
+      name: 'addonsToInstall',
+      message: 'Which addons would you like to install?',
+      choices: [
+        {
+          checked: true,
+          name: 'flexi-default-styles',
+          value: { name: 'flexi-default-styles' }
+        },
+        {
+          checked: true,
+          name: 'flexi-dsl',
+          value: { name: 'flexi-dsl' }
+        },
+        {
+          checked: true,
+          name: 'flexi-layouts',
+          value: { name: 'flexi-layouts' }
+        },
+        {
+          checked: false,
+          name: 'flexi-sustain',
+          value: { name: 'flexi-sustain' }
+        },
       ]
-    });
+    })
+      .then((selected) => {
+        return selected.addonsToInstall;
+      });
   }
 };
