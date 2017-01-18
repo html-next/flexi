9c94b10f6b975f3a6d80cb474b7fadb333fc84dc
Robert Wagner <rwwagner90@gmail.com>
2017-01-18 07:40:39 -0500

    Add validate, and some separators with descriptions

--- blueprints/flexi/index.js
+++ blueprints/flexi/index.js
@@ -1,3 +1,5 @@
+var inquirer = require('inquirer');
+
 /*jshint node:true*/
 module.exports = {
   description: 'Choose which addons to install',
@@ -25,27 +27,38 @@ module.exports = {
       name: 'addonsToInstall',
       message: 'Which addons would you like to install?',
       choices: [
+        new inquirer.Separator('flexi-default-styles - Default flexi styles'),
         {
           checked: true,
           name: 'flexi-default-styles',
           value: { name: 'flexi-default-styles' }
         },
+        new inquirer.Separator('flexi-dsl - Converts attributes to classes'),
         {
           checked: true,
           name: 'flexi-dsl',
           value: { name: 'flexi-dsl' }
         },
+        new inquirer.Separator('flexi-layouts - Layout service and grids'),
         {
           checked: true,
           name: 'flexi-layouts',
           value: { name: 'flexi-layouts' }
         },
+        new inquirer.Separator('flexi-sustain - Recyclable components (Ember <= 2.9)'),
         {
           checked: false,
+          message: '',
           name: 'flexi-sustain',
           value: { name: 'flexi-sustain' }
         },
-      ]
+      ],
+      validate: (answer) => {
+        if (answer.length < 1) {
+          return 'You must choose at least one addon.';
+        }
+        return true;
+      }
     })
       .then((selected) => {
         return selected.addonsToInstall;
