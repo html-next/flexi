2818c9e5860e3228991f4cf7e7ea9f323b9de4a8
Robert Wagner <rwwagner90@gmail.com>
2017-02-01 14:09:58 -0500

    2.0.0-beta.1

--- blueprints/flexi/index.js
+++ blueprints/flexi/index.js
@@ -31,26 +31,26 @@ module.exports = {
         {
           checked: true,
           name: 'flexi-default-styles',
-          value: { name: 'flexi-default-styles' }
+          value: { name: '@html-next/flexi-default-styles', target: '2.0.0-beta.1' }
         },
         new inquirer.Separator('flexi-dsl - Converts attributes to classes'),
         {
           checked: true,
           name: 'flexi-dsl',
-          value: { name: 'flexi-dsl' }
+          value: { name: '@html-next/flexi-dsl', target: '2.0.0-beta.1' }
         },
         new inquirer.Separator('flexi-layouts - Layout service and grids'),
         {
           checked: true,
           name: 'flexi-layouts',
-          value: { name: 'flexi-layouts' }
+          value: { name: '@html-next/flexi-layouts', target: '2.0.0-beta.1' }
         },
         new inquirer.Separator('flexi-sustain - Recyclable components (Ember <= 2.9)'),
         {
           checked: false,
           message: '',
           name: 'flexi-sustain',
-          value: { name: 'flexi-sustain' }
+          value: { name: '@html-next/flexi-sustain', target: '2.0.0-beta.1' }
         },
       ],
       validate: (answer) => {
