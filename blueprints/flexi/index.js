a434b53528d2f0f8af3271dca8402e07228a8a8c
Kyle Bishop <kybishop@gmail.com>
2017-02-02 00:52:20 -0800

    patch version matching for sub-addons

--- blueprints/flexi/index.js
+++ blueprints/flexi/index.js
@@ -31,26 +31,26 @@ module.exports = {
         {
           checked: true,
           name: 'flexi-default-styles',
-          value: { name: '@html-next/flexi-default-styles', target: '2.0.0-beta.8' }
+          value: { name: '@html-next/flexi-default-styles', target: '~2.0.0-beta.9' }
         },
         new inquirer.Separator('flexi-dsl - Converts attributes to classes'),
         {
           checked: true,
           name: 'flexi-dsl',
-          value: { name: '@html-next/flexi-dsl', target: '2.0.0-beta.7' }
+          value: { name: '@html-next/flexi-dsl', target: '~2.0.0-beta.7' }
         },
         new inquirer.Separator('flexi-layouts - Layout service and grids'),
         {
           checked: true,
           name: 'flexi-layouts',
-          value: { name: '@html-next/flexi-layouts', target: '2.0.0-beta.7' }
+          value: { name: '@html-next/flexi-layouts', target: '~2.0.0-beta.7' }
         },
         new inquirer.Separator('flexi-sustain - Recyclable components (Ember <= 2.9)'),
         {
           checked: false,
           message: '',
           name: 'flexi-sustain',
-          value: { name: '@html-next/flexi-sustain', target: '2.0.0-beta.6' }
+          value: { name: '@html-next/flexi-sustain', target: '~2.0.0-beta.6' }
         },
       ],
       validate: (answer) => {
