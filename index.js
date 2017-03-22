7b12460e98f8f8dbe863c50390e879a227ec9aab
Kyle Bishop <kybishop@gmail.com>
2017-03-21 18:54:17 -0700

    interdependency checks

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 /* eslint-env node */
 'use strict';
 
-var getValidatedFlexiConfig = require('@html-next/flexi-config/lib/get-validated-flexi-config');
-
-function assert(statement, test) {
-  if (!test) {
-    throw new Error(statement);
-  }
-}
+const CHALK = require('chalk');
+const SEMVER = require('semver');
 
 module.exports = {
   name: 'flexi',
 
-  included: function(app, parentAddon) {
+  included(app, parentAddon) {
     this._super.included.apply(this, arguments);
 
     // Quick fix for add-on nesting
@@ -31,31 +26,49 @@ module.exports = {
     }
 
     if (!parentAddon && typeof app.import !== 'function') {
-      throw new Error('flexi is being used within another addon or engine and is' +
-        ' having trouble registering itself to the parent application.');
+      throw new Error('flexi is being used within another addon or engine and is'
+        + ' having trouble registering itself to the parent application.');
     }
 
     this.app = app;
+
+    this._checkDependencyCompatibility(this.project);
+
     return app;
   },
 
-  isDevelopingAddon: function() {
-    return false;
-  },
+  _checkDependencyCompatibility(project) {
+    let packageVersionByName = {};
 
-  _flexiConfig: null,
-  flexiConfig: function() {
-    if (!this._flexiConfig) {
-      this._flexiConfig = getValidatedFlexiConfig(this.project.root);
+    for (let addon of project.addons) {
+      packageVersionByName[addon.pkg.name] = addon.pkg.version;
     }
 
-    return this._flexiConfig;
+    let flexiConfigVersion = packageVersionByName['@html-next/flexi-config'];
+    let flexiDefaultStylesVersion = packageVersionByName['@html-next/flexi-default-styles'];
+    let flexiDslVersion = packageVersionByName['@html-next/flexi-dsl'];
+    let flexiLayoutsVersion = packageVersionByName['@html-next/flexi-layouts'];
+
+    if (flexiConfigVersion && SEMVER.lt(flexiConfigVersion, '2.0.0-rc.1')) {
+      this._warnUser('flexi-config < v2.0.0-rc.1; consider upgrading to at least v2.0.0-rc.1');
+    }
+    if (flexiDefaultStylesVersion && SEMVER.lt(flexiDefaultStylesVersion, '2.0.0-rc.2')) {
+      this._warnUser('flexi-default-styles < v2.0.0-rc.2;'
+                     + ' consider upgrading to at least v2.0.0-rc.2');
+    }
+    if (flexiDslVersion && SEMVER.lt(flexiDslVersion, '2.0.0-rc.2')) {
+      this._warnUser('flexi-dsl < v2.0.0-rc.2; consider upgrading to at least v2.0.0-rc.2');
+    }
+    if (flexiLayoutsVersion && SEMVER.lt(flexiLayoutsVersion, '2.0.0-rc.2')) {
+      this._warnUser('flexi-layouts < v2.0.0-rc.2; consider upgrading to at least v2.0.0-rc.2');
+    }
   },
 
-  config: function() {
-    var org = this._super.config.apply(this, arguments);
+  _warnUser(string) {
+    this.ui.writeLine(CHALK.yellow(`WARNING: ${string}`));
+  },
 
-    org.flexi = this.flexiConfig();
-    return org;
+  isDevelopingAddon() {
+    return false;
   }
 };
