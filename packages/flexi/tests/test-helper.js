b3b4caa956b790616a253a9d033c5a446ab1e609
Chris Thoburn <runspired@users.noreply.github.com>
2022-03-22 12:08:00 -0700

    modernize app and tests

--- packages/flexi/tests/test-helper.js
+++ packages/flexi/tests/test-helper.js
@@ -1,10 +1,15 @@
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
+
 import { setApplication } from '@ember/test-helpers';
 
 import { start } from 'ember-qunit';
 
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'frontend/app';
+import config from 'frontend/config/environment';
 
 setApplication(Application.create(config.APP));
 
-start();
+setup(QUnit.assert);
+
+start({ setupTestIsolationValidation: true });
